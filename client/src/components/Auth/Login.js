a149e9735ccdc7ca45756d4d7c64671e4b691b50
Vitalii Sihydyn <kappao5o6o7@gmail.com>
2019-05-02 13:36:45 +0300

    Build Header component

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -45,6 +45,7 @@ const Login = ({ classes }) => {
         onFailure={onFailure}
         clientId={process.env.REACT_APP_OAUTH_CLIENT_ID}
         theme="dark"
+        buttonText="Login with Google"
       />
     </div>
   );
