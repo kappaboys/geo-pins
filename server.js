ed2c279334ffbc384e26bd4e41d3ce98825fe4d4
Vitalii Sihydyn <kappao5o6o7@gmail.com>
2019-07-05 09:58:37 +0300

    Define env variable PORT

--- server.js
+++ server.js
@@ -34,7 +34,7 @@ const server = new ApolloServer({
   },
 });
 
-server.listen()
+server.listen({ port: process.env.PORT || 4000 })
   .then(({ url }) => {
     console.log(`Server is running on ${url}`);
   });
